eff20a0f4181490df749767529c010b62a516a96
William Sundin <willesundin99@gmail.com>
2021-10-05 11:16:14 +0200

    cameracontroller
    maxheigt added and made it modular

--- PHJK The Game/Assets/Scripts/CameraController.cs
+++ PHJK The Game/Assets/Scripts/CameraController.cs
@@ -8,18 +8,24 @@ public class CameraController : MonoBehaviour
 
     [SerializeField] private Transform playerTransform;
     private Camera _camera;
+    [SerializeField] private float maxHiegtOfCamera = 411;
+    [SerializeField] private float minHeightOfCamera = 0;
 
     // Update is called once per frame
     void Update()
     {
 
-        if (playerTransform.position.y > 0f)
+        if (playerTransform.position.y > minHeightOfCamera)
         {
             transform.position = new Vector3(transform.position.x, playerTransform.position.y, -10f); 
         }
-        else if (playerTransform.position.y < 0f)
+        else if (playerTransform.position.y < minHeightOfCamera)
         {
-            transform.position = new Vector3(transform.position.x, 0, -10f);
+            transform.position = new Vector3(transform.position.x, minHeightOfCamera, -10f);
+        }
+        if (playerTransform.position.y  > maxHiegtOfCamera)
+        {
+            transform.position = new Vector3(transform.position.x, maxHiegtOfCamera, -10);
         }
 
         
